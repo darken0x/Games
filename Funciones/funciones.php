6503eeeb0045f57305c6a9b9ea5ec065f600cf42
darken0x <dosuper@yahoo.com>
2011-09-28 11:21:20 -0500

    CONECCION CON BASE DE DATOS
    SE EMPEZO CON LA CONECCION A LA BASE DE DATOS DE FORMA EXITOSA
    
    Signed-off-by: darken0x <dosuper@yahoo.com>

--- Funciones/funciones.php
+++ Funciones/funciones.php
@@ -4,7 +4,22 @@
 		$host = "DIEGO-566EF7A94";
 		$user = "sa";
 		$pass = "uno";
-		$con = mssql_connect($host, $user, $pass) or die ("Unable to connect!");
-		return $con;
+		$rConnect = mssql_connect($host, $user, $pass) or die ("Unable to connect!");
+
+		return $rConnect;
+	}
+
+	function fBaseDatos($psqlcon, $pbasedatos)
+	{		if ($psqlcon > 0)
+			$rBaseDatos = mssql_select_db($pbasedatos,$psqlcon);
+		else
+			$rBaseDatos = "e";
+
+		return $rBaseDatos;
+	}
+
+	function fSelecciona($tabla,$cam001,$cam002){
+		$rSelecciona = "select * from $tabla where $cam001 = $cam002";
+		return $rSelecciona;
 	}
 ?>
--- menu.php
+++ menu.php
@@ -1,10 +1,17 @@
-
+<?php include ("./Funciones/funciones.php"); ?>
 <?php
-	$con = fConnect();
+	$sqlcon = fConnect();
+	$basedatos = fBaseDatos($sqlcon, "GameTrophy");
+	if ($basedatos == 1)
+	{
+		$sql = fSelecciona("C_MENU","ID_STATUS",1);
+		$rsMenu = mssql_query($sql);
+	}
+	mssql_close($sqlcon);
 ?>
 
 <table>
 	<tr>
-		<td>Hola Mundo</td>
+		<td>Hola Mundo <?php echo $rsMenu; ?></td>
 	</tr>
 </table>
